58b5df77c599d7b6c288ad5ad9c715ffb932978a
Budimir Filipovic <budimkafin@gmail.com>
2022-03-17 16:37:40 +0100

    Dodat check za special code, i uslovi u consoli za kretanje cursora

--- kernel/kbd.c
+++ kernel/kbd.c
@@ -3,48 +3,71 @@
 #include "defs.h"
 #include "kbd.h"
 
-int
-kbdgetc(void)
+int kbdgetc(void)
 {
 	static uint shift;
 	static uchar *charcode[4] = {
-		normalmap, shiftmap, ctlmap, ctlmap
-	};
+		normalmap, shiftmap, ctlmap, ctlmap};
 	uint st, data, c;
 
 	st = inb(KBSTATP);
-	if((st & KBS_DIB) == 0)
+	if ((st & KBS_DIB) == 0)
 		return -1;
 	data = inb(KBDATAP);
 
-	if(data == 0xE0){
+	// ulazi samo kad je home
+	if (data == 0xE0)
+	{
+		// shift setuje 6 bit na 1 (tj kao da je escape pritisnut)
 		shift |= E0ESC;
 		return 0;
-	} else if(data & 0x80){
+	}
+	// ulazi ako nije prinisnut karakter
+	else if (data & 0x80)
+	{
 		// Key released
 		data = (shift & E0ESC ? data : data & 0x7F);
 		shift &= ~(shiftcode[data] | E0ESC);
 		return 0;
-	} else if(shift & E0ESC){
+	}
+	else if (shift & E0ESC)
+	{
 		// Last character was an E0 escape; or with 0x80
 		data |= 0x80;
 		shift &= ~E0ESC;
 	}
 
+	// SHIFT = 0
+	//
+	//
 	shift |= shiftcode[data];
 	shift ^= togglecode[data];
+
 	c = charcode[shift & (CTL | SHIFT)][data];
-	if(shift & CAPSLOCK){
-		if('a' <= c && c <= 'z')
+
+	// ulazi u mod
+	if ((c == 'c' || c == 'C') && (shift & (SHIFT | ALT)))
+	{
+		c = SPECIAL_CODE('C');
+	}
+
+	// izlazi iz moda
+	if ((c == 'p' || c == 'P') && (shift & (SHIFT | ALT)))
+	{
+		c = SPECIAL_CODE('P');
+	}
+
+	if (shift & CAPSLOCK)
+	{
+		if ('a' <= c && c <= 'z')
 			c += 'A' - 'a';
-		else if('A' <= c && c <= 'Z')
+		else if ('A' <= c && c <= 'Z')
 			c += 'a' - 'A';
 	}
 	return c;
 }
 
-void
-kbdintr(void)
+void kbdintr(void)
 {
 	consoleintr(kbdgetc);
 }
